e62570aeef692fde21eed3674a73444ab256f8ae
Asen <asen.trichkov1@gmail.com>
2023-02-20 02:29:13 +0200

    refactor

---
+++ crypto_bot/coingeckoClient.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const axios = require("axios")
+
+class Api {
+    constructor(currency) {
+        this.currency = currency
+    }
+
+    async getAssetPrice(coin) {
+        try {
+            console.log(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${this.currency}`);
+            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${this.currency}`)
+
+            return response.data[coin]
+        } catch (err) {
+            console.log('aaaaaaa')
+            console.error(err)
+        }
+    }
+}
+
+module.exports = Api
--- crypto_bot/index.js
+++ crypto_bot/index.js
@@ -1,13 +1,13 @@
-const fs = require('fs')
-const Path = require('path')  
 require('dotenv').config()
-const axios = require("axios")
 const { Telegraf } = require('telegraf')
+const Api = require('./coingeckoClient.js')
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
+
+const api = new Api('usd')
 
 let chatId
 bot.command('start', ctx => {
@@ -18,35 +18,24 @@ bot.command('start', ctx => {
     if (process.env.PRICE_NOTIFICATION_ENABLED && chatId) {
         if (tickTock) { clearInterval(tickTock) }
         tickTock = setInterval(() => {
-            let rate
-            axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`)
-            .then(response => {
-                rate = response.data.bitcoin
-                const message = `Hello, price update .. $${rate.usd.toLocaleString()} 💰`
-                bot.telegram.sendMessage(chatId, message)
-            })
+            const rate = api.getAssetPrice('bitcoin')
+            const message = `Hello, price update .. $${rate.usd.toLocaleString()} 💰`
+
+            bot.telegram.sendMessage(chatId, message)
         }, process.env.PRICE_NOTIFICATION_INTERVAL*60*60*1000);
     }
 })
 
-bot.command('bitcoin', ctx => {
-    let rate
-    axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`)
-    .then(response => {
-        rate = response.data.bitcoin
-        const message = `Hello, current bitcoin price is $${rate.usd.toLocaleString()} 💰`
-        ctx.reply(message)
-    })
+bot.command('bitcoin', async ctx => {
+    const rate = await api.getAssetPrice('bitcoin')
+    const message = `Hello, current bitcoin price is $${rate.usd.toLocaleString()} 💰`
+    ctx.reply(message)
 })
 
-bot.command('ethereum', ctx => {
-    let rate
-    axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
-    .then(response => {
-        rate = response.data.ethereum
-        const message = `Hello, current ethereum price is $${rate.usd.toLocaleString()} 💰`
-        ctx.reply(message)
-    })
+bot.command('ethereum', async ctx => {
+    const rate = await api.getAssetPrice('ethereum')
+    const message = `Hello, current ethereum price is $${rate.usd.toLocaleString()} 💰`
+    ctx.reply(message)
 })
 
 bot.launch()
