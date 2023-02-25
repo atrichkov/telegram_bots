2f815cc7ad48e8dd1359a78c9add4b4b310a59e9
Asen <asen.trichkov1@gmail.com>
2023-02-25 04:41:23 +0200

    FEAT: check price for percentage change

--- crypto_bot/index.js
+++ crypto_bot/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const Api = require('./coingeckoClient.js')
@@ -19,22 +21,33 @@ bot.command('start', ctx => {
         if (tickTock) { clearInterval(tickTock) }
         tickTock = setInterval(() => {
             const rate = api.getAssetPrice('bitcoin')
-            const message = `Hello, price update .. $${rate.usd.toLocaleString()} 💰`
+            const message = `Hello, price update .. $ ${rate.usd.toLocaleString()} 💰`
 
             bot.telegram.sendMessage(chatId, message)
         }, process.env.PRICE_NOTIFICATION_INTERVAL*60*60*1000);
     }
+
+    let lastPrice = null
+    setInterval(async () => {
+        const rate = await api.getAssetPrice('bitcoin')
+        const percentageChange = (((lastPrice - rate.usd) / lastPrice) * 100).toFixed(2)
+        
+        if (lastPrice && Math.abs(percentageChange) > 10)
+            bot.telegram.sendMessage(chatId, `bitcoin price is changed by ${percentageChange}% to ${rate.usd}`)
+
+        lastPrice = rate.usd
+    }, 3 * 60 * 60)
 })
 
 bot.command('bitcoin', async ctx => {
     const rate = await api.getAssetPrice('bitcoin')
-    const message = `Hello, current bitcoin price is $${rate.usd.toLocaleString()} 💰`
+    const message = `Hello, current bitcoin price is $ ${rate.usd.toLocaleString()} 💰`
     ctx.reply(message)
 })
 
 bot.command('ethereum', async ctx => {
     const rate = await api.getAssetPrice('ethereum')
-    const message = `Hello, current ethereum price is $${rate.usd.toLocaleString()} 💰`
+    const message = `Hello, current ethereum price is $ ${rate.usd.toLocaleString()} 💰`
     ctx.reply(message)
 })
 
