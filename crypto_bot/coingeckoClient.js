f1499275c607ccd4af15a127a5466f7491c3901e
Asen <asen.trichkov1@gmail.com>
2023-03-28 22:46:29 +0300

    refactoring

--- crypto_bot/coingeckoClient.js
+++ crypto_bot/coingeckoClient.js
@@ -1,22 +1,24 @@
 'use strict'
 
-const axios = require("axios")
+const axios = require('axios')
 
 class Api {
-    constructor(currency) {
-        this.currency = currency
-    }
+  constructor(currency) {
+    this.currency = currency
+  }
 
-    async getAssetPrice(coin) {
-        try {
-            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${this.currency}`)
+  async getAssetPrice(coin) {
+    try {
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${this.currency}`
+      )
 
-            return response.data[coin]
-        } catch (err) {
-            console.log('aaaaaaa')
-            console.error(err)
-        }
+      return response.data[coin]
+    } catch (err) {
+      console.log('aaaaaaa')
+      console.error(err)
     }
+  }
 }
 
 module.exports = Api
--- crypto_bot/index.js
+++ crypto_bot/index.js
@@ -6,49 +6,61 @@ const Api = require('./coingeckoClient.js')
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 bot.catch((err, ctx) => {
-    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
 
 const api = new Api('usd')
 
 let chatId
-bot.command('start', ctx => {
-    chatId = ctx.chat.id
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! \nI respond to /bitcoin or /ethereum. Please try it.', {})
+bot.command('start', (ctx) => {
+  chatId = ctx.chat.id
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    'Hello there! \nI respond to /bitcoin or /ethereum. Please try it.',
+    {}
+  )
 
-    let tickTock
-    if (process.env.PRICE_NOTIFICATION_ENABLED && chatId) {
-        if (tickTock) { clearInterval(tickTock) }
-        tickTock = setInterval(() => {
-            const rate = api.getAssetPrice('bitcoin')
-            const message = `Hello, price update .. $ ${rate.usd.toLocaleString()} 💰`
-
-            bot.telegram.sendMessage(chatId, message)
-        }, process.env.PRICE_NOTIFICATION_INTERVAL*60*60*1000);
+  let tickTock
+  if (process.env.PRICE_NOTIFICATION_ENABLED && chatId) {
+    if (tickTock) {
+      clearInterval(tickTock)
     }
+    tickTock = setInterval(() => {
+      const rate = api.getAssetPrice('bitcoin')
+      const message = `Hello, price update .. $ ${rate.usd.toLocaleString()} 💰`
 
-    let lastPrice = null
-    setInterval(async () => {
-        const rate = await api.getAssetPrice('bitcoin')
-        const percentageChange = (((lastPrice - rate.usd) / lastPrice) * 100).toFixed(2)
-        
-        if (lastPrice && Math.abs(percentageChange) > 10)
-            bot.telegram.sendMessage(chatId, `bitcoin price is changed by ${percentageChange}% to ${rate.usd}`)
+      bot.telegram.sendMessage(chatId, message)
+    }, process.env.PRICE_NOTIFICATION_INTERVAL * 60 * 60 * 1000)
+  }
 
-        lastPrice = rate.usd
-    }, 3 * 60 * 60)
-})
-
-bot.command('bitcoin', async ctx => {
+  let lastPrice = null
+  setInterval(async () => {
     const rate = await api.getAssetPrice('bitcoin')
-    const message = `Hello, current bitcoin price is $ ${rate.usd.toLocaleString()} 💰`
-    ctx.reply(message)
+    const percentageChange = (
+      ((lastPrice - rate.usd) / lastPrice) *
+      100
+    ).toFixed(2)
+
+    if (lastPrice && Math.abs(percentageChange) > 10)
+      bot.telegram.sendMessage(
+        chatId,
+        `bitcoin price is changed by ${percentageChange}% to ${rate.usd}`
+      )
+
+    lastPrice = rate.usd
+  }, 3 * 60 * 60)
 })
 
-bot.command('ethereum', async ctx => {
-    const rate = await api.getAssetPrice('ethereum')
-    const message = `Hello, current ethereum price is $ ${rate.usd.toLocaleString()} 💰`
-    ctx.reply(message)
+bot.command('bitcoin', async (ctx) => {
+  const rate = await api.getAssetPrice('bitcoin')
+  const message = `Hello, current bitcoin price is $ ${rate.usd.toLocaleString()} 💰`
+  ctx.reply(message)
+})
+
+bot.command('ethereum', async (ctx) => {
+  const rate = await api.getAssetPrice('ethereum')
+  const message = `Hello, current ethereum price is $ ${rate.usd.toLocaleString()} 💰`
+  ctx.reply(message)
 })
 
 bot.launch()
