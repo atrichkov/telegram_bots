88d6a9e852e501f1cd33f61f67639ccb5a491a2f
Asen <asen.trichkov1@gmail.com>
2023-04-01 22:12:33 +0300

    dea fixes

--- dea/index.js
+++ dea/index.js
@@ -6,95 +6,101 @@ const commandArgsMiddleware = require('./commandArgs')
 
 const token = process.env.TELEGRAM_BOT_TOKEN
 if (token === undefined) {
-    throw new Error('BOT_TOKEN must be provided!')
+  throw new Error('BOT_TOKEN must be provided!')
 }
 const bot = new Telegraf(token)
 
-bot.use(commandArgsMiddleware());
+bot.use(commandArgsMiddleware())
 
 /**
- * @param {Object} ctx 
- * @param {string} message 
+ * @param {Object} ctx
+ * @param {string} message
  */
 function reply(ctx, message) {
-    ctx.reply(message, {
-        parse_mode: 'Markdown',
-        disable_web_page_preview: true
-    })
+  ctx.reply(message, {
+    parse_mode: 'Markdown',
+    disable_web_page_preview: true,
+  })
 }
 
-bot.command('start', ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! \nI respond to \n \
+bot.command('start', (ctx) => {
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    'Hello there! \nI respond to \n \
      /update command to load latest DEA list \
-     /check command followed by valid email address. Please try it.', {})
+     /check command followed by valid email address. Please try it.',
+    {}
+  )
 })
 
-bot.command('update', ctx => {
-    const url = 'https://raw.githubusercontent.com/kslr/disposable-email-domains/master/list.json'
-    const filename = 'list.json'
+bot.command('update', (ctx) => {
+  const url =
+    'https://raw.githubusercontent.com/kslr/disposable-email-domains/master/list.json'
+  const filename = 'list.json'
 
-    try {
-        https.get(url, res => {
-            if (res.statusCode < 200 || res.statusCode >= 300) {
-                return reply(ctx, 'New DEA list the cannot be downloaded!')
-            }
-    
-            const writeStream = fs.createWriteStream(filename)
-         
-            res.pipe(writeStream)
-         }).on('error', (err) => {
-            fs.unlink(filename) // Delete the file async. (But we don't check the result)
-            console.error(err.message)
-            throw err;
-         }).on('finish', () => {
-    
-         })
-    } catch(err) {
-        console.error(err)
-        return reply(ctx, 'New DEA list the cannot be downloaded!')
-    }
+  try {
+    https
+      .get(url, (res) => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          return reply(ctx, 'New DEA list the cannot be downloaded!')
+        }
+
+        const writeStream = fs.createWriteStream(filename)
+
+        res.pipe(writeStream)
+      })
+      .on('error', (err) => {
+        fs.unlink(filename) // Delete the file async. (But we don't check the result)
+        console.error(err.message)
+        throw err
+      })
+      .on('finish', () => {})
+  } catch (err) {
+    console.error(err)
+    return reply(ctx, 'New DEA list the cannot be downloaded!')
+  }
 })
 
-bot.command('check', async ctx => {
-    try {
-        console.debug(ctx.state.command) // command object
-        let message = ''
+bot.command('check', async (ctx) => {
+  try {
+    console.debug(ctx.state.command) // command object
+    let message = ''
 
-        let email = null
-        if (ctx.state.command) {
-            if (!ctx.state.command.args.length) {
-                return reply(ctx, 'Email is not provided!')
-            }
-            email = ctx.state.command.args[0].trim()
-        }
-
-        const t0 = performance.now();
-
-        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-            message = 'Please provide valid email!'
-            return reply(ctx, message)
-        }
-
-        const emailDomain = email.split('@')[1]
-        const data = fs.readFileSync('list.json', 'utf8')
-
-        if (data) {
-            list = JSON.parse(data)
-
-            if (list.includes(emailDomain)) {
-                message = `${email} is disposable email`
-            } else {
-                message = `${email} is *not* disposable email`
-            }
-        }
-
-        reply(ctx, message)
-
-        const t1 = performance.now();
-        console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
-    } catch(err) {
-        console.error('Error: ', err)
+    let email = null
+    if (ctx.state.command) {
+      if (!ctx.state.command.args.length) {
+        return reply(ctx, 'Email is not provided!')
+      }
+      email = ctx.state.command.args[0].trim()
     }
+
+    const t0 = performance.now()
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      message = 'Please provide valid email!'
+      return reply(ctx, message)
+    }
+
+    const emailDomain = email.split('@')[1]
+    const data = fs.readFileSync('list.json', 'utf8')
+
+    if (data) {
+      list = JSON.parse(data)
+
+      if (list.includes(emailDomain)) {
+        message = `${email} is disposable email`
+      } else {
+        message = `${email} is *not* disposable email`
+      }
+    }
+
+    reply(ctx, message)
+
+    const t1 = performance.now()
+    console.log(`Call to doSomething took ${t1 - t0} milliseconds.`)
+  } catch (err) {
+    console.error('Error: ', err)
+  }
 })
 
 bot.launch()
